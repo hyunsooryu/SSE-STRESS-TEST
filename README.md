96899734ce6e136e26a60cf3c10707964af0ecde
hyeonju-kim <41038031+hyunsooryu@users.noreply.github.com>
2024-04-12 02:03:09 +0900

    부하테스터기 생성
    부하테스트 애플리케이션

---
+++ README.md
@@ -0,0 +1,16 @@
+# SSE STRESS TEST
+
+### Spec
+
+* mvn
+* gatling.io
+* JDK 17
+
+
+### SSE 를 자유롭게 테스트할 수 있는 환경을 제공한다.
+
+
+
+
+
+
---
+++ src/test/java/sse/SSESimulation.java
@@ -0,0 +1,44 @@
+package sse;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+import java.time.Duration;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+
+
+public class SSESimulation extends Simulation {
+
+    private HttpProtocolBuilder httpProtocol = http.baseUrl("http://localhost:8080")
+            .acceptHeader("application/json");
+    private ScenarioBuilder scn = scenario("SSE SERVER 부하 테스트")
+            .exec(sse("Connect").connect("/sub"));
+
+
+    //Load Simulation
+    {
+        setUp(
+                /**
+                scn.injectOpen(
+                        nothingFor(4), // 1
+                        atOnceUsers(500),
+                        rampUsers(1000).during(10)
+                       // rampUsers(10).during(5), // 3
+                       // constantUsersPerSec(20).during(15), // 4
+                       // constantUsersPerSec(20).during(15).randomized(), // 5
+                       // rampUsersPerSec(10).to(20).during(10), // 6
+                       // rampUsersPerSec(10).to(20).during(10).randomized(), // 7
+                       // stressPeakUsers(10000).during(20) // 8
+                ).protocols(httpProtocol));
+                 **/
+
+
+                        scn.injectOpen(
+                                rampUsers(10000).during(Duration.ofSeconds(30))
+                        )
+                ).protocols(httpProtocol).maxDuration(Duration.ofMinutes(1));
+    }
+}
